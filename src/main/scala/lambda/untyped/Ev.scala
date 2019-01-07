765efbfbb83f25ebbe464e9c484dfd65ae224fa9
Odomontois <odomontois@gmail.com>
2019-01-08 01:36:57 +0300

    Evaluations

--- src/main/scala/lambda/untyped/Ev.scala
+++ src/main/scala/lambda/untyped/Ev.scala
@@ -2,16 +2,45 @@ package lambda.untyped
 
 import cats.Eval
 import lambda.untyped.Lam.Name
+import cats.syntax.apply._
+import Eval.now
+import terms._
 
 sealed trait Ev[A]
 
 object Ev {
-  final case class Val[A](a: Eval[A]) extends Ev[A]
-  final case class Fun[A](f: Eval[A => Eval[A]]) extends Ev[A]
+  type Run[A] = Map[Name, Eval[Ev[A]]] => Eval[Ev[A]]
 
-  implicit def lambda[A]: Lam[Ev[A]] = new Lam[Ev[A]] {
-    def lam(name: Name, term: Ev[A]): Ev[A] = ???
-    def app(f: Ev[A], x: Ev[A]): Ev[A] = ???
-    def v(name: Name): Ev[A] = ???
+  def run[A](run: Run[A]): Either[String, A] =
+    run(Map.empty).value match {
+      case Val(a1) => Right(a1.value)
+      case Error(str) => Left(str)
+      case Fun(_) => Left("result is a function")
+    }
+
+  def lift[A](f: A => A): Run[A] = m => now(Fun(now((ex: Eval[Ev[A]]) => ex.flatMap {
+    case Val(ea) => ea.map(a => Val(now(f(a))))
+    case err@Error(_) => now(err)
+    case Fun(_) => now(Error("could apply lifted function to function"))
+  })))
+
+  def pure[A](x: A): Run[A] = m => now(Val(now(x)))
+
+  def runInt(term: Run[Int]): Either[String, Int] = run(term ! lift(_ + 1) ! pure(0))
+  def runBool(term: Run[Boolean]): Either[String, Boolean] = run(term ! pure(true) ! pure(false))
+
+  final case class Val[A](a: Eval[A]) extends Ev[A]
+  final case class Error[A](s: String) extends Ev[A]
+  final case class Fun[A](f: Eval[Eval[Ev[A]] => Eval[Ev[A]]]) extends Ev[A]
+
+  implicit def lambda[A]: Lam[Run[A]] = new Lam[Run[A]] {
+    def lam(name: Name, term: Run[A]): Run[A] =
+      m => now(Fun[A](now(x => term(m + (name -> x)))))
+    def app(f: Run[A], x: Run[A]): Run[A] = m => f(m).flatMap {
+      case Error(str) => now(Error(str))
+      case Val(ea) => now(Error(s"cound not apply ${ea.value} as function"))
+      case Fun(ef) => ef.flatMap(ff => ff(x(m)))
+    }
+    def v(name: Name): Run[A] = m => m.getOrElse(name, now(Error(s"not found $name")))
   }
 }
--- src/main/scala/lambda/untyped/LApp.scala
+++ src/main/scala/lambda/untyped/LApp.scala
@@ -10,7 +10,7 @@ object LApp extends App {
   def steps(x: Term): Stream[String] =
     x.stepBN match {
       case Some(x1) => x.to[String] #:: steps(x1)
-      case None     => Stream(x.to[String])
+      case None => Stream(x.to[String])
     }
 
   def printsteps(x: Term) = steps(x).foreach(println)
@@ -19,8 +19,15 @@ object LApp extends App {
   def `2^4`[A: Lam] = pow ! (two, four)
   def `5==5`[A: Lam] = eqn ! (five, five)
   def `4!`[A: Lam] = fact ! four
+  def `8!`[A: Lam] = fact ! (plus ! (five, three))
+  def `10=9`[A: Lam] = eqn ! (plus ! (five, five), plus ! (four, five))
+  def `10=10`[A: Lam] = eqn ! (plus ! (five, five), plus ! (three, plus ! (four, three)))
 
   printred(`2^4`[Term])
   printred(`5==5`[Term])
   printred(`4!`[Term])
+
+  println(Ev.runInt(`8!`[Ev.Run[Int]]))
+  println(Ev.runBool(`10=9`[Ev.Run[Boolean]]))
+  println(Ev.runBool(`10=10`[Ev.Run[Boolean]]))
 }
